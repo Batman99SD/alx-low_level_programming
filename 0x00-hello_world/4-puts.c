b7a3c76ce2d743978c46db9ecf006ea0e583efc3
Batman99SD <azozkrg1@gmail.com>
2023-08-10 15:48:21 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 /**
  * main - main block
  * Return: 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
