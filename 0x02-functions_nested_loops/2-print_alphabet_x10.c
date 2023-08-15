2aba8cbbd84060de21de329bd40506547d5d0dac
Batman99SD <azozkrg1@gmail.com>
2023-08-15 16:04:27 -0700

    tentimes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - print_alphabet_x10 ten times.
+ *
+ * Return: 0.
+ */
+void print_alphabet_x10(void)
+
+{
+int n = 1;
+
+while (n < 10)
+{
+    char c = 'a';
+
+    while (c < 'z')
+    {
+        putchar(c);
+        c++;
+    }
+    n++;
+    putchar('\n');
+}
+}
