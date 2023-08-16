e59daee927cc50ffbf689a06458d02108f3b0e59
Batman99SD <azozkrg1@gmail.com>
2023-08-15 17:16:27 -0700

    main4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - _islower: print 1 if lowercase.
+ * @c: cheks input of function
+ *  Return: return 1 if 'c' is lower
+ *          otherwise always 0.
+ **/
+int _islower(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+if (c >= 97 && c <= 122
+return (1);
+return (0);
+}
+
