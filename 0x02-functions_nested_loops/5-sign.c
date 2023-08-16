b22246ea8190d1c236a2e505ee592d07c3764214
Batman99SD <azozkrg1@gmail.com>
2023-08-15 17:45:36 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_sign - print_sign: prints a + if 'n' is positive
+*@n: takes input from function.
+*Return: return 1 if positive
+*/
+int print_sign(int n)
+{
+if (n > 0)
+_putchar('+');
+return (1);
+if (n == 0)
+_putchar('0');
+return (0);
+if (n < 0)
+_putchar('-');
+return (-1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,10 @@
 #ifndef HEADER_NAME_H
 #define HEADER_NAME_H
-
-#include <stdio.h>
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
+int print_sign(int n);
 int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _putchar(char c);
 #endif
+
