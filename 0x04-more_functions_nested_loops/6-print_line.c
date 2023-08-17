b867931d8e92c7a4b4d53db0f2ee82a81aa46da4
Batman99SD <azozkrg1@gmail.com>
2023-08-17 09:35:52 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - print_line: prints a '_' for n times.
+ *
+ * @n: checkes for input.
+ *
+ * Return: returns nothung.
+ */
+void print_line(int n)
+{
+	int n;
+
+	for (n > 0; n++)
+	{
+		_putchar('_');
+	}
+	_putchar(10);
+}
