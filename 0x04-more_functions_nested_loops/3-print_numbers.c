e94050cde3004522b180f70cfbfbb700be0daabd
Batman99SD <azozkrg1@gmail.com>
2023-08-17 07:35:13 -0700

    printnum

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.n"
+/**
+ * print_numbers - print_numbers: print number followed by new line.
+ *
+ * Return: return 0.
+ */
+void print_numbers(void)
+{
+	int n = 48;
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
+}
