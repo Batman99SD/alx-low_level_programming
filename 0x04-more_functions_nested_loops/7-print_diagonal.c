ebd228aa2057bfb26bbe3aa89ce5876162069dd2
Batman99SD <azozkrg1@gmail.com>
2023-08-17 12:51:29 -0700

    space

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,15 +8,15 @@
  */
 void print_diagonal(int n)
 {
-	int spc, bsh;
+	int bsh, spc;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (spc = 0; spc < n; spc++)
+		for (bsh = 0; bsh < n; bsh++)
 		{
-			for (bsh = 0; bsh < spc; bsh++)
+			for (spc = 0; spc < bsh; spc++)
 				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
