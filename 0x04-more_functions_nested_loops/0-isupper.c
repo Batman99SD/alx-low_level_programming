5b5a82ea51e4014d05675bd12cee89660696d8c7
Batman99SD <azozkrg1@gmail.com>
2023-08-17 06:21:30 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - _isupper: print 1 if uppercase.
+ * @c: cheks input of function
+ *  Return: return 1 if 'c' is lower
+ *          otherwise always 0.
+ **/
+int _isupper(int c)
+{
+	if (c >= A && c <= Z)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+isupper
+Write a function that checks for uppercase character.
+
+ isdigit
+Write a function that checks for a digit (`0` through `9`).
