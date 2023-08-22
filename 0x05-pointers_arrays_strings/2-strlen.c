5f3ae56b020dde4fb1a2ff4d49c179d3964cff43
Batman99SD <azozkrg1@gmail.com>
2023-08-21 17:31:19 -0700

    size

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = (sizeof(s) / sizeof(s[0]))-1;
+
+		return (i);
+}
