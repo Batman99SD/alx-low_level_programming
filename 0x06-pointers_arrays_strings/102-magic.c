136701dba30856134c4e3c4ef64d5155fc90e2ce
Batman99SD <azozkrg1@gmail.com>
2023-08-24 17:52:53 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -22,7 +22,7 @@ int main(void)
 	printf("a[2] = %p\n", &a[2]);
 	printf("p    = %p\n", (p + 5));
 	printf("a[3] = %p\n", &a[3]);
-	*9p + 5) = 98;
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
