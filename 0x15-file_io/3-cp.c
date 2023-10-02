4200828614e7073e85f9f3ecd40e8a8696cd2ac5
Batman99sd <azozkrg1@gmail.com>
2023-10-02 16:23:46 -0700

    3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define BUF_SIZE 1024
+
+void exit_with_error(int code, const char *message)
+{
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(code);
+}
+
+int main(int argc, char *argv[])
+{
+	const char *file_from, *file_to;
+	int fd_from, fd_to;
+	char buffer[BUF_SIZE];
+	ssize_t bytes_read, bytes_written;
+
+	if (argc != 3)
+		exit_with_error(97, "Usage: cp file_from file_to");
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		close(fd_from);
+		exit(99);
+	}
+
+	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		close(fd_to);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
+
